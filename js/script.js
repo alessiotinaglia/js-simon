da53db8644a0e039a5d29a126a20868a5cc40487
alessiotinaglia <alessio.tina1998@gmail.com>
2024-11-11 17:07:05 +0100

    pagina non ricaricata

--- js/script.js
+++ js/script.js
@@ -7,12 +7,13 @@ const numbersList = document.getElementById("numbers-list")
 const answersForm = document.getElementById("answers-form")
 const message = document.getElementById("message")
 const instructions = document.getElementById("instructions")
-
-// richiamo di funzioni 
-
+const form = document.getElementById('answers-form');
+const button = document.querySelector("button.ntn");
+const numeriUtente = document.querySelector('.form-control');
+const numeriGiusti = [];
 
 // conto alla rovescia
-let secondi = 11;
+let secondi = 6;
 const tempo = setInterval(function () {
     if (secondi > 1) {
         countdown.innerHTML = --secondi;
@@ -20,19 +21,30 @@ const tempo = setInterval(function () {
         clearInterval(tempo);
         countdown.remove();
         numbersList.remove();
+        answersForm.classList.remove("d-none");
     }
-}, 1000);
+}, 1500);
 
 console.log(countdown);
 
 // creare 5 numeri randm con un array 
 const numeriRandom = [];
-for (let i = 0; i < 5; i++){
+for (let i = 0; i < 5; i++) {
     numeriRandom.push(generaNumeroRandom(1, 100))
 }
 console.log(numeriRandom);
-
 // stampato su crome e convertito in stringa 
 numbersList.innerHTML = numeriRandom.join(" ");
 
 
+// non ricarica la pagina + commento stampato 
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    for (let i = 0; i < numeriUtente.length; i++) {
+        if (numeriRandom.includes(numeriUtente[i])) {
+            numeriGiusti.push(numeriUtente[i]); 
+            message.innerHTML = numeriGiusti;
+            console.log(numeriUtente);
+        }
+    }
+});
