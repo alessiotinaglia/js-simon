7c7eb45ed5192edf4ab80068bf4aa6c7342d4cd5
alessiotinaglia <alessio.tina1998@gmail.com>
2024-11-11 15:37:30 +0100

    creato 5 num random

--- js/script.js
+++ js/script.js
@@ -1,29 +1,38 @@
 "use strict"
 console.clear();
 
-// Dichiaro le variabili
-
+// Dichiaro le variabili del HTML
 const countdown = document.getElementById("countdown")
 const numbersList = document.getElementById("numbers-list")
 const answersForm = document.getElementById("answers-form")
 const message = document.getElementById("message")
 const instructions = document.getElementById("instructions")
 
-// conto alla rovescia
+// richiamo di funzioni 
 
+
+// conto alla rovescia
 let secondi = 11;
-const tempo = setInterval(function (){
+const tempo = setInterval(function () {
     if (secondi > 1) {
         countdown.innerHTML = --secondi;
     } else {
         clearInterval(tempo);
         countdown.remove();
+        numbersList.remove();
     }
 }, 1000);
 
 console.log(countdown);
 
-// creare 5 numeri randm con arrei 
+// creare 5 numeri randm con un array 
+const numeriRandom = [];
+for (let i = 0; i < 5; i++){
+    numeriRandom.push(generaNumeroRandom(1, 100))
+}
+console.log(numeriRandom);
 
+// stampato su crome e convertito in stringa 
+numbersList.innerHTML = numeriRandom.join(" ");
 
 
